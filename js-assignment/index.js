69b72da1542e6d7cc2b5ba2868200c0c0522ecb3
arissetyawan1 <aris.setyawan354313@gmail.com>
2021-09-28 11:25:13 +0700

    js-assignment

---
+++ js-assignment/index.js
@@ -0,0 +1,20 @@
+// Question Introduction #1
+// 1
+let sample1 = "budi is waeboo and he have";
+let sample2 = 20;
+let sample3 = "collecetion of anime video";
+console.log(`${sample1} ${sample2} ${sample3}`);
+// 2
+console.log(sample1.length + sample2.toString.length + sample3.length);
+// 3
+let sample4 = "but budi denailed it.";
+let sample5 = sample4 + "even though he was caught watching".toUpperCase();
+console.log(sample5);
+// 4
+let sqrT = 5;
+console.log(Math.pow(sqrT, 5));
+// 5
+let lastSample = "hours per day";
+console.log(typeof Boolean === lastSample);
+// 6
+console.log(typeof lastSample);
